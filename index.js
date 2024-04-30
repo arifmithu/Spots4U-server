858bae856c4b611e1b247c6edbb13a6483173df7
arifmithu <mdarifhossainmithu@gmail.com>
2024-04-30 20:46:33 +0600

    added delete a spot

--- index.js
+++ index.js
@@ -93,12 +93,6 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/spots/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await allSpots.deleteOne(query);
-      res.send(result);
-    });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
